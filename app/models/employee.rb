c4bb10977d868e5778112fb3f311c03aa0052f5b
Janusz Kupras <januszkupras@gmail.com>
2022-06-06 14:58:20 +0200

    add kudos controller and views

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -5,7 +5,7 @@ class Employee < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :giver_kudos, class_name: 'Kudos',
-                            foreign_key: 'giver_id'
+                         foreign_key: 'giver_id'
   has_many :receiver_kudos, class_name: 'Kudos',
                             foreign_key: 'receiver_id'
 end
