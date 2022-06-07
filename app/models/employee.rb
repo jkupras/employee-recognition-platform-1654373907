1cc9fea91eb8842ee96ea053fa6e9870a1259c81
Janusz Kupras <januszkupras@gmail.com>
2022-06-07 07:50:36 +0200

    fix emploee association

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,8 +4,8 @@ class Employee < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :giver_kudos, class_name: 'Kudos',
+  has_many :giver_kudos, class_name: 'Kudo',
                          foreign_key: 'giver_id'
-  has_many :receiver_kudos, class_name: 'Kudos',
+  has_many :receiver_kudos, class_name: 'Kudo',
                             foreign_key: 'receiver_id'
 end
