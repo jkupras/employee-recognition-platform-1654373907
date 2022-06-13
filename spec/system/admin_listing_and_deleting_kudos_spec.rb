9b42aca6e37fc62d3b74ce9269991dd1471061de
Janusz Kupras <januszkupras@gmail.com>
2022-06-13 11:45:28 +0200

    add admin kudos controller and views for list and delete

---
+++ spec/system/admin_listing_and_deleting_kudos_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'the admin listing and deleting', type: :system do
+  let(:admin) { create(:admin) }
+  let!(:kudo) { create(:kudo) }
+
+  before do
+    sign_in admin
+  end
+
+  it 'listing kudos' do
+    visit admins_authenticated_root_path
+    click_link 'All Kudos list'
+    expect(page).to have_content 'Log out'
+    expect(page).to have_content 'Kudos'
+    expect(page).to have_content 'Title'
+    expect(page).to have_content kudo.title
+    expect(page).to have_content 'Content'
+    expect(page).to have_content kudo.content
+    expect(page).to have_link 'Show'
+    expect(page).to have_link 'Destroy'
+  end
+
+  it 'deleting kudos' do
+    visit admins_authenticated_root_path
+    click_link 'All Kudos list'
+    click_link 'Destroy', match: :first
+    expect(page).to have_content 'Kudo was successfully destroyed.'
+  end
+end
