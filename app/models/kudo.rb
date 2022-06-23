470d34a57363b7970c6b649d440794035dcf2455
Janusz Kupras <januszkupras@gmail.com>
2022-06-23 21:58:54 +0200

    add company_value and kudo association

--- app/models/kudo.rb
+++ app/models/kudo.rb
@@ -1,6 +1,7 @@
 class Kudo < ApplicationRecord
   belongs_to :giver, class_name: 'Employee'
   belongs_to :receiver, class_name: 'Employee'
+  belongs_to :company_value
   validates :title, presence: true
   validates :content, presence: true
   validate :number_of_available_kudos_cannot_be_less_then_zero
