c34e6a48ddf8f4b90663b7783820a2332d474e9b
Janusz Kupras <januszkupras@gmail.com>
2022-06-28 15:09:25 +0200

    add Reward model specs'

--- spec/models/reward_spec.rb
+++ spec/models/reward_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Reward, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:reward) { create(:reward) }
+
+  describe '#title' do
+    it { is_expected.to validate_presence_of(:title) }
+  end
+
+  describe '#description' do
+    it { is_expected.to validate_presence_of(:description) }
+  end
+
+  describe '#price' do
+    it { is_expected.to validate_presence_of(:price) }
+    it { is_expected.to validate_numericality_of(:price).is_greater_than_or_equal_to(1) }
+  end
 end
