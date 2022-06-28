82e94c649e2c90b1d1fd8afa6effb22095211241
Janusz Kupras <januszkupras@gmail.com>
2022-06-28 13:46:53 +0200

    add reward model migration and factory

---
+++ spec/factories/rewards.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :reward do
+    title { 'Book' }
+    description { "'Życie i śmierć na Drodze Umarłych' autor Tomasz Grzywaczewski " }
+    price { 29.99 }
+  end
+end
