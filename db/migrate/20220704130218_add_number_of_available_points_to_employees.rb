721b10953c1e4432d44f2af7a319aa33408aba9f
Janusz Kupras <januszkupras@gmail.com>
2022-07-04 15:14:29 +0200

    migration adding column number_of_available_points to employees table

---
+++ db/migrate/20220704130218_add_number_of_available_points_to_employees.rb
@@ -0,0 +1,5 @@
+class AddNumberOfAvailablePointsToEmployees < ActiveRecord::Migration[6.1]
+  def change
+    add_column :employees, :number_of_available_points, :decimal, null: false, default: 0, precision: 8, scale: 2
+  end
+end
